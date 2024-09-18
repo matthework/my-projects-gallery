24457290ee5fe0179f0876275c6d9dc9db007a13
Matt Wang <matthework@hotmail.com>
2024-09-18 23:12:48 +1200

    1. Run frontend and backend at same port. 2. Add scripts to build and run in production.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 import express from "express";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import projectRoutes from "./routes/project.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allow to accept json data for req.body
 
 app.use("/api/projects", projectRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
